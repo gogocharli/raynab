a332ef654f31ae46ad78dbbb6a09150c25db6fe4
Charles <thexrayone@icloud.com>
2021-12-25 21:36:54 -0500

    feat(transactionsCommand): :credit_card: add transaction details

--- src/transactions.tsx
+++ src/transactions.tsx
@@ -40,18 +40,25 @@ function TransactionItem({ transaction }: { transaction: TransactionDetail }) {
       actions={
         <ActionPanel title="Inspect Budget">
           {/* TODO: Add more actions and filters */}
-          <PushAction title="Show Transaction" target={<TransactionDetails />} />
+          <PushAction title="Show Transaction" target={<TransactionDetails transaction={transaction} />} />
         </ActionPanel>
       }
     />
   );
 }
 
-function TransactionDetails() {
+function TransactionDetails({ transaction }: { transaction: TransactionDetail }) {
   /* TODO: Show amount, category, account, payee, etc */
+  const markdown = `
+  # ${transaction.amount > 0 ? 'Inflow to' : 'Outflow from'} ${transaction.account_name}
+
+  - **Amount**: ${formatPrice(transaction.amount)} CAD
+  - **Payee**: ${transaction.payee_name ?? 'Not Specified'}
+  - **Category**: ${transaction.category_name ?? 'Not Specified'}
+  `;
   return (
     <Detail
-      markdown="Transaction Payee"
+      markdown={markdown}
       actions={
         <ActionPanel>
           <CopyToClipboardAction title="Copy Transaction Amount" content="Dollar Billz" />
@@ -59,4 +66,8 @@ function TransactionDetails() {
       }
     />
   );
+}
+
+function formatPrice(price: number) {
+  return `${Number(price / 1000).toFixed(2)}`;
 }
