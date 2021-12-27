4267d7214f63a1a8b9e75fe943edc70413732d26
Charles <thexrayone@icloud.com>
2021-12-26 23:58:23 -0500

    feat(transactionsCommand): :credit_card: add sorting order to transaction list

--- src/transactions.tsx
+++ src/transactions.tsx
@@ -1,6 +1,7 @@
-import { Icon, List, ActionPanel, Color, PushAction, Detail, CopyToClipboardAction } from '@raycast/api';
+import { Icon, List, ActionPanel, Color, PushAction, Detail, CopyToClipboardAction, randomId } from '@raycast/api';
 import { SWRConfig } from 'swr';
 
+import { useState } from 'react';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import localizedFormat from 'dayjs/plugin/localizedFormat';
@@ -22,21 +23,68 @@ export default function Command() {
   );
 }
 
+type GroupTypes = 'category' | 'payee' | 'account';
+type GroupNames = `${GroupTypes}_name`;
+interface Group<T> {
+  id: string;
+  title: string;
+  items: T[];
+}
+
 function TransactionList() {
   const [activeBudgetId] = useSharedState('activeBudgetId', '');
   const { data: transactions, isValidating } = useTransactions(activeBudgetId);
 
-  /* TODO: Hold new state to transition between different views (By Category, By Account, etc) */
+  const [groupBy, setGroupBy] = useState<GroupNames | null>(null);
+
+  /* TODO: Hold new state for filters */
+  // const [filter, setFilter] = useState(null);
+
+  const groups =
+    groupBy &&
+    transactions?.reduce<Map<string, Group<TransactionDetail>>>((groupMap, currentTransaction) => {
+      const groupName = currentTransaction[groupBy] ?? '';
+
+      if (!groupMap.has(groupName)) {
+        groupMap.set(groupName, {
+          id: `${groupName}-${randomId()}`,
+          title: groupName,
+          items: [],
+        });
+      }
+
+      const previousGroup = groupMap.get(groupName) as Group<TransactionDetail>;
+      groupMap.set(groupName, { ...previousGroup, items: [...previousGroup?.items, currentTransaction] });
+
+      return groupMap;
+    }, new Map());
+
+  const handleGrouping = (groupType: GroupNames | null) => () => setGroupBy(groupType);
+
   return (
     <List isLoading={isValidating}>
-      {transactions?.map((t) => (
-        <TransactionItem transaction={t} key={t.id} />
-      ))}
+      {groups
+        ? Array.from(groups).map(([, group]) => (
+            <List.Section
+              title={group.title}
+              key={group.id}
+              children={group.items.map((t) => (
+                <TransactionItem transaction={t} key={t.id} onGrouping={handleGrouping} />
+              ))}
+            />
+          ))
+        : transactions?.map((t) => <TransactionItem transaction={t} key={t.id} onGrouping={handleGrouping} />)}
     </List>
   );
 }
 
-function TransactionItem({ transaction }: { transaction: TransactionDetail }) {
+function TransactionItem({
+  transaction,
+  onGrouping,
+}: {
+  transaction: TransactionDetail;
+  onGrouping: (groupType: GroupNames | null) => () => void;
+}) {
   const mainIcon =
     transaction.amount > 0
       ? { source: Icon.ChevronUp, tintColor: Color.Green }
@@ -49,8 +97,13 @@ function TransactionItem({ transaction }: { transaction: TransactionDetail }) {
       accessoryTitle={dayjs(transaction.date).fromNow()}
       actions={
         <ActionPanel title="Inspect Budget">
-          {/* TODO: Add more actions and filters */}
+          {/* TODO: Add filters */}
           <PushAction title="Show Transaction" target={<TransactionDetails transaction={transaction} />} />
+          <ActionPanel.Submenu title="Group by">
+            <ActionPanel.Item title="Category" icon={Icon.TextDocument} onAction={onGrouping('category_name')} />
+            <ActionPanel.Item title="Payee" icon={Icon.TextDocument} onAction={onGrouping('payee_name')} />
+            <ActionPanel.Item title="Account" icon={Icon.TextDocument} onAction={onGrouping('account_name')} />
+          </ActionPanel.Submenu>
         </ActionPanel>
       }
     />
