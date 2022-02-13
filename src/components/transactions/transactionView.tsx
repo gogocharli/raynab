8dc9eeb18411cf7a9df76f29aac0f8d21debf1e5
Charles <thexrayone@icloud.com>
2022-02-13 11:15:21 -0500

    fix(transactionsCommand): :bug: add handling for transaction periods with no transactions listed

--- src/components/transactions/transactionView.tsx
+++ src/components/transactions/transactionView.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useReducer } from 'react';
-import { List } from '@raycast/api';
+import { List, showToast, ToastStyle } from '@raycast/api';
 
 import { useSharedState } from '@lib/useSharedState';
 import { useTransactions } from '@lib/ynab';
@@ -25,11 +25,46 @@ export function TransactionView() {
     initView
   );
 
+  const { collection, group, sort, filter } = state;
+
   useEffect(() => {
+    // Showing an empty list will prevent users from accessing actions and will be stuck
+    // We progressively back off in order to not fetch unnecessary data
+    // This might cause problems for budgets with no transactions in the past year
+    // TODO add a view for > 1 year, change to a different fallback model?
+
+    const showBackupToast = async (fallback: Period) => {
+      await showToast(
+        ToastStyle.Failure,
+        `No results for the past ${timeline}`,
+        `Falling back to the last ${fallback}`
+      );
+    };
+
+    if (transactions.length == 0) {
+      let fallbackTimeline: Period;
+      switch (timeline) {
+        case 'day':
+          fallbackTimeline = 'week';
+          break;
+        case 'week':
+          fallbackTimeline = 'month';
+          break;
+        case 'month':
+          fallbackTimeline = 'quarter';
+          break;
+        default:
+          fallbackTimeline = 'year';
+          break;
+      }
+
+      setTimeline(fallbackTimeline);
+      showBackupToast(fallbackTimeline);
+      return;
+    }
+
     dispatch({ type: 'reset', initialCollection: transactions });
   }, [timeline]);
-
-  const { collection, group, sort, filter } = state;
 
   return (
     <TransactionProvider dispatch={dispatch} state={{ group, sort, filter, timeline }} onTimelineChange={setTimeline}>
