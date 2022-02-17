43f274a5a6db132f134d01b05d77ece4de6b240a
Charles <thexrayone@icloud.com>
2022-02-17 18:01:03 -0500

    feat(hooks): :zap: update useSharedState hook to not revalidate preemptively

--- src/hooks/useSharedState.ts
+++ src/hooks/useSharedState.ts
@@ -1,8 +1,10 @@
 import useSWR, { type KeyedMutator } from 'swr';
 
 export function useSharedState<T>(key: string, initial: T): [T | undefined, KeyedMutator<T>] {
-  const { data: state, mutate: setState } = useSWR<T>(key, null, {
+  const { data: state = initial, mutate: setState } = useSWR<T>(key, null, {
     fallbackData: initial,
+    refreshInterval: 0,
+    revalidateIfStale: false,
   });
   return [state, setState];
 }
